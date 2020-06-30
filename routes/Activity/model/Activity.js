aa8547e88782f3bd677eef1ab27b4c738f76288e
panduhjohn <johnrstilwell@codeimmersives.com>
2020-06-29 22:27:20 -0400

    added to userController

--- routes/Activity/model/Activity.js
+++ routes/Activity/model/Activity.js
@@ -1,12 +1,11 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 const ActivitySchema = mongoose.Schema({
-    name:{type:String,trim:true,},
-    id:{type:Number,trim:true,},
-    location:{
-        address:{type:String,trim:true}
+    name: { type: String, trim: true },
+    id: { type: Number, trim: true },
+    location: {
+        address: { type: String, trim: true },
     },
-    cost:{type:String,trim:true},
-    thumb:{type:String,trim:true}
-
-})
+    cost: { type: String, trim: true },
+    thumb: { type: String, trim: true },
+});
--- routes/user/userController/userController.js
+++ routes/user/userController/userController.js
@@ -1,57 +1,65 @@
-const User = require("../model/User");
-const bcrypt = require("bcryptjs");
-const dbErrorHelper = require("../../lib/dbErrorHelper/dbErrorHelper");
-const jwtHelper = require("../authHelpers/jwtHelper");
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const dbErrorHelper = require('../../lib/dbErrorHelper/dbErrorHelper');
+const jwtHelper = require('../authHelpers/jwtHelper');
+
 module.exports = {
-  signUp: async (req, res) => {
-    try {
-      let createdUser = new User({
-        email: req.body.email,
-        password: req.body.password,
-        username: req.body.username,
-      });
-      let genSalt = await bcrypt.genSalt(12);
-      let hashedPassword = await bcrypt.hash(createdUser.password, genSalt);
-      createdUser.password = hashedPassword;
-      await createdUser.save();
-      res.json({
-        message: "User created",
-      });
-    } catch (e) {
-      res.status(500).json({
-        message: dbErrorHelper(e),
-      });
-    }
-  },
-
-  login: async (req, res) => {
-    console.log(req.body);
     
-    try {
-      let foundUser = await User.findOne({
-        email: req.body.email,
-      }).select("-__v -userCreated");
-      if (foundUser === null) {
-        throw Error("User not found, please sign up.");
-      }
-      let comparedPassword = await jwtHelper.comparePassword(
-        req.body.password,
-        foundUser.password
-      );
-      if (comparedPassword === 409) {
-        throw Error("Check your email and password.");
-      }
-      let jwtToken = await jwtHelper.createJwtToken(foundUser);
-      res.json({
-        jwtToken: jwtToken,
-      });
-    } catch (e) {
-      console.log(e);
-      
-      res.status(500).json({
-        message: dbErrorHelper(e),
-      });
-    }
-  },
-};
+    signUp: async (req, res) => {
+        try {
+            let createdUser = new User({
+                // name: req.body.name,
+                username: req.body.username,
+                email: req.body.email,
+                password: req.body.password,
+                // city: req.body.city,
+                // state: req.body.state,
+                // zipCode: req.body.zipCode,
+            });
+            let genSalt = await bcrypt.genSalt(12);
+            let hashedPassword = await bcrypt.hash(
+                createdUser.password,
+                genSalt
+            );
+            createdUser.password = hashedPassword;
+            await createdUser.save();
+            res.json({
+                message: 'User created',
+            });
+        } catch (e) {
+            res.status(500).json({
+                message: dbErrorHelper(e),
+            });
+        }
+    },
 
+    login: async (req, res) => {
+        console.log(req.body);
+
+        try {
+            let foundUser = await User.findOne({
+                email: req.body.email,
+            }).select('-__v -userCreated');
+            if (foundUser === null) {
+                throw Error('User not found, please sign up.');
+            }
+            let comparedPassword = await jwtHelper.comparePassword(
+                req.body.password,
+                foundUser.password
+            );
+            if (comparedPassword === 409) {
+                throw Error('Check your email and password.');
+            }
+            let jwtToken = await jwtHelper.createJwtToken(foundUser);
+            res.json({
+                jwtToken: jwtToken,
+            });
+        } catch (e) {
+            console.log(e);
+
+            res.status(500).json({
+                message: dbErrorHelper(e),
+            });
+        }
+    },
+};

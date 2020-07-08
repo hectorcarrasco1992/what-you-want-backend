5033451bbe47cfe4abf9136fc1bbd29cfa2ae7a0
panduhjohn <johnrstilwell@codeimmersives.com>
2020-07-08 16:02:00 -0400

    added findFriends

--- routes/user/users.js
+++ routes/user/users.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { signUp, login } = require('./userController/userController');
+const { signUp, login, findFriend } = require('./userController/userController');
 
 /* GET users listing. */
 router.post('/login', login);
 router.post('/register', signUp);
+router.post('/find-friend', findFriend)
 
 module.exports = router;

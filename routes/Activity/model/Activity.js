6c03ac98892f39e65a7f4604539160739b2bdfcb
panduhjohn <johnrstilwell@codeimmersives.com>
2020-07-13 00:02:39 -0400

    commented out code removed

--- routes/Activity/model/Activity.js
+++ routes/Activity/model/Activity.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const ActivitySchema = mongoose.Schema({
     name: { type: String, trim: true },
-    cuisines:{type:String,trim:true},
+    cuisines: { type: String, trim: true },
     location: {
         address: { type: String, trim: true },
     },
     cost: { type: String, trim: true },
     thumb: { type: String, trim: true },
-    apiID:{type:Number,trim:true}
+    apiID: { type: Number, trim: true },
 });
 
-module.exports = mongoose.model("Activity",ActivitySchema)
+module.exports = mongoose.model('Activity', ActivitySchema);
--- routes/lib/passport/user-passport.js
+++ routes/lib/passport/user-passport.js
@@ -2,9 +2,12 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../../user/model/User');
 const keys = process.env.JWT_USER_SECRET_KEY;
+
 const jwtOpts = {};
+
 jwtOpts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 jwtOpts.secretOrKey = keys;
+
 const userJWTLoginStrategy = new JwtStrategy(jwtOpts, async (payload, done) => {
     const userEmail = payload.email;
     try {
@@ -20,4 +23,5 @@ const userJWTLoginStrategy = new JwtStrategy(jwtOpts, async (payload, done) => {
         return done(error, false);
     }
 });
+
 module.exports = userJWTLoginStrategy;
